ee51cfa1923fd26bf446b192fa3b6d225eb71755
ChandraMouli-M-2002 <chandramouli9245@gmail.com>
2025-09-17 11:04:41 +0530

    multer

--- guvi-blog/routers/blogRoutes.js
+++ guvi-blog/routers/blogRoutes.js
@@ -1,9 +1,27 @@
 const express=require('express');
 const router=express.Router();
+const multer=require("multer");
 
-router.get('/',function(req,res){
-    if(!req.user) return res.redirect('/login')
-    res.end('ok');
+const storage=multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"./public/uploads/");
+    },
+    filename:(req,file,cb)=>{
+        // cb(null,file.filename);
+        cb(null, Date.now() + "-" + file.originalname);
+    }
+})
+
+const upload=multer({storage})
+
+router.get('/create',function(req,res){
+    res.render('createblog');
+});
+
+router.post('/create',upload.single("coverimage"), function(req,res){
+    console.log(req.body);
+    console.log(req.file);
+    return res.render('createblog');
 });
 
 module.exports=router;
--- guvi-blog/routers/userRoutes.js
+++ guvi-blog/routers/userRoutes.js
@@ -2,11 +2,13 @@ const express=require('express');
 const router=express.Router();
 const {handleUserLogin,handleUserSignup}= require('../controllers/userController')
 
-router.get('/:id', function(req,res){
-    res.end("Route is Under Const");
+router.get('/logout', function(req,res){
+    return res.clearCookie('Token').redirect('/');
 })
 
 router.post('/login',handleUserLogin);
 
 router.post('/signup',handleUserSignup);
+
+
 module.exports=router;
